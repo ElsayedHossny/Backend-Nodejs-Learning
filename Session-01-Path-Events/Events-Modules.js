fd09ee958844844dc49621012fa051d8fb3fb99d
Elsayed Hossny <elsayedhossny90@gmail.com>
2026-07-30 14:09:54 +0300

    Add initial implementations for event and path modules; create file operations in Fs-Modules

---
+++ Session-01-Path-Events/Events-Modules.js
@@ -0,0 +1,24 @@
+// const x = require("node:events");
+
+// const emitter1 = new x.EventEmitter();
+
+// console.log(emitter1);
+
+// const events = require("node:events");
+
+// const emitter = new events();
+
+// const { EventEmitter } = require("node:events");
+
+// const emitter = new EventEmitter();
+// const emitter = new events.EventEmitter();
+
+// const functionEvent = (...arr) => {
+//   console.log("Event one", arr);
+// };
+
+// emitter.once("event1", functionEvent);
+
+// console.log(emitter.eventNames());
+// emitter.emit("event1", 2, 3, 2);
+// console.log(emitter.eventNames());
